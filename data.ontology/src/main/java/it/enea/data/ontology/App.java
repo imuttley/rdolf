cc150f90f2e4acd8b7d92bf93a7644ef1296059d
Tommaso Nicosia <tommaso.nicosia@enea.it>
2017-12-17 21:56:01 +0100

    class model

--- data.ontology/src/main/java/it/enea/data/ontology/App.java
+++ data.ontology/src/main/java/it/enea/data/ontology/App.java
@@ -1,5 +1,8 @@
 package it.enea.data.ontology;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.rdf.model.Model;
@@ -8,6 +11,7 @@ import org.apache.jena.rdf.model.RDFList;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.vocabulary.DC;
+import org.apache.jena.vocabulary.RDF;
 
 /**
  * Test GRAPH
@@ -24,7 +28,9 @@ public class App {
 		OntModel model = ModelFactory.createOntologyModel();
 
 		model.setNsPrefix("myo", nsURI);
-
+		model.setNsPrefix("dc", DC.NS);
+		model.setNsPrefix("purl", "http://purl.org/");
+		
 		OntClass td = model.createClass(nsURI + "TimeDomain");
 		td.addLabel("TimeDomain", "en");
 		td.addVersionInfo("0.0");
@@ -39,14 +45,17 @@ public class App {
 		// create the resource
 		Resource pippo = fd.createIndividual(nsURI + "pippo");
 		Resource pluto = td.createIndividual(nsURI + "pluto");
-		Resource right = model.createResource("http://purl.org/cc-by");
+		Resource right = model.createResource("http://purl.org/" + "cc-by");
 		pippo.addLiteral(DC.title,"pippo");
 		pluto.addLiteral(DC.title,"pluto");
 		pippo.addProperty(DC.rights, right);
 		pluto.addProperty(DC.rights, right);
 		
+		Resource root = model.createResource(nsURI + "filexy.wav");
+		
 		Resource cs = (Resource) model.createList(new RDFNode[] { pippo, pluto });
-
+		
+		root.addProperty(DC.relation,cs);
 		
 		
 		// add the property
@@ -54,5 +63,12 @@ public class App {
 		// model.createLiteral("an RDF vocabulary for mammals", "en"));
 		model.write(System.out, "RDF/XML");
 
+		try {
+			model.write(new FileOutputStream("ttlfile.ttl"), "TURTLE");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
