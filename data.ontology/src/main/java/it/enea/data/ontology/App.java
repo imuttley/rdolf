d8e110e66115b1c4795527551892fe0b3a03ad7a
Tommaso Nicosia <tommaso.nicosia@enea.it>
2017-12-17 20:30:35 +0100

    Configure and test

--- data.ontology/src/main/java/it/enea/data/ontology/App.java
+++ data.ontology/src/main/java/it/enea/data/ontology/App.java
@@ -1,13 +1,31 @@
 package it.enea.data.ontology;
 
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.vocabulary.DC;
+
 /**
- * Hello world!
+ * Test GRAPH
  *
  */
 public class App 
 {
+	static final String nsURI    = "http://neruon4web.palermo.enea.it/";
+    static final String family     = "dolphin";
+
+  
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+       // create an empty Model
+        Model model = ModelFactory.createDefaultModel();
+
+        // create the resource
+        Resource graph = model.createResource(nsURI);
+
+        // add the property
+        graph.addLiteral(DC.description, "an RDF vocabulary for mammals");
+
     }
 }
